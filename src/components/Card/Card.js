b75b8d499bf8da4dafabffb01daf49d5d7c47721
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2022-09-30 16:29:32 +0600

    added user info

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
     const { image, title, age, time } = props.activity;
     return (
         <div className='bg-[#FFD1D1] p-5 rounded-lg font-semibold'>
-            <img className='rounded-lg' src={image} alt="" />
+            <img className='rounded-lg w-80 h-[13.5rem]' src={image} alt="" />
             <h2 className='text-xl mt-2'>{title}</h2>
             <h3>For Age: {age}</h3>
             <h3>Time Required: {time}</h3>
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,17 +1,10 @@
 import React from 'react';
+import UserInfo from '../UserInfo/UserInfo';
 
 const SideBar = () => {
     return (
-        <div className='bg-[#FFE3E1]'>
-            <div className='flex items-center'>
-                <img className='w-16' src={require('./image/user-avatar.png')} alt="Image of user" />
-                <div className='font-semibold ml-3'>
-                    <h3>Jhankar Mahbub Jr</h3>
-                    <div className='flex items-center'>
-                        <i class="fa-solid fa-location-dot"></i><p className='ml-1'>Sydney, Australia</p>
-                    </div>
-                </div>
-            </div>
+        <div className='w-72 p-5 bg-[#FFE3E1]'>
+            <UserInfo></UserInfo>
         </div>
     );
 };
