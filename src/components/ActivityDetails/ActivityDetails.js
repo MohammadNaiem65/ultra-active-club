3f207c00b5a2976a3dd88a9b9a39687a11a72c7d
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2022-09-30 17:08:24 +0600

    completed the sidebar

--- src/components/ActivityDetails/ActivityDetails.js
+++ src/components/ActivityDetails/ActivityDetails.js
@@ -3,7 +3,15 @@ import React from 'react';
 const ActivityDetails = () => {
     return (
         <div>
-
+            <h2 className='text-xl font-bold'>Add A Break</h2>
+            <div className='bg-[#FFF5E4] mt-4 rounded-lg flex justify-between p-2 gap-1 text-md'>
+                <h2 className='font-semibold'>Activity Time: </h2>
+                <h2>100m</h2>
+            </div>
+            <div className='bg-[#FFF5E4] mt-4 rounded-lg flex justify-between p-2 gap-1 text-md'>
+                <h2 className='font-semibold'>Break Time: </h2>
+                <h2>100m</h2>
+            </div>
         </div>
     );
 };
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ActivityDetails from '../ActivityDetails/ActivityDetails';
 import AddBreak from '../AddBreak/AddBreak';
 import UserInfo from '../UserInfo/UserInfo';
 
@@ -9,6 +10,9 @@ const SideBar = () => {
             <hr className='border-2 border-[#FFF5E4] bg-[#FFF5E4] rounded-xl mx-auto my-6 w-3/4' />
             <AddBreak></AddBreak>
             <hr className='border-2 border-[#FFF5E4] bg-[#FFF5E4] rounded-xl mx-auto my-6 w-3/4' />
+            <ActivityDetails></ActivityDetails>
+            <hr className='border-2 border-[#FFF5E4] bg-[#FFF5E4] rounded-xl mx-auto my-6 w-3/4' />
+            <button className='w-full mt-3 py-2 font-bold bg-[#FF9494] hover:bg-[#f2a4b1] hover:text-white rounded-lg'>Activity Completed</button>
         </div>
     );
 };
