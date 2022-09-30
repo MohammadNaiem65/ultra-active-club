8def2fb1f3fe7882d9bc8cf45bbab286467d80c6
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2022-09-30 11:45:18 +0600

    revised the fake json file and worked on the header and solved some mistakes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './Header.css';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div className='w-4/5 mx-auto mt-6'>
-            <h1 id='title' className='text-5xl font-bold'><i class="fa-solid fa-chart-line"></i> Daily Activities</h1>
-            <h2 className='text-2xl font-bold mt-16'>Select Today's Activities</h2>
+        <div>
+            <div className='w-4/5 mx-auto mt-6'>
+                <h1 id='title' className='text-5xl font-bold'><i className="fa-solid fa-chart-line"></i> Daily Activities</h1>
+                <h2 className='text-2xl font-bold mt-16'>Select Today's Activities</h2>
+                <div className='border-2 border-[#FFE3E1] bg-[#FFE3E1] rounded-xl mt-1 w-2/4'>  </div>
+            </div>
         </div>
     );
 };
